20eac9e9ea07a6668b72e60698ee61ed5391f649
Ibrahim Moghul <34140989+imoghul@users.noreply.github.com>
2021-11-26 01:42:38 +0000

    formatted

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from numpy import mean
 fileNames = glob.glob("*RAW.csv")
 fileNames.sort()
 
-dtToMin = lambda y,mon,d,h,m,s:(525600 * y + 43800 * mon + 1440 * d + 60 * h + m + s / 60)
+dtToMin = lambda y, mon, d, h, m, s: (525600 * y + 43800 * mon + 1440 * d + 60
+                                      * h + m + s / 60)
+
+
 # print("Choose a file: ")
 # for i in range(len(fileNames)):
 #   print(i,": ",fileNames[i])
 # x=int(input("Enter file number: "))
 # fileNames=[fileNames[2]]
-def readTime(dt):  # takes input in the format of      Hour:Min:Sec (A/P)M Month/Day/Year
+def readTime(
+    dt
+):  # takes input in the format of      Hour:Min:Sec (A/P)M Month/Day/Year
     dt = dt.split(" ")
     d = dt[2]
     d_arr = (d.split("/"))
@@ -46,35 +51,36 @@ def calc(fileName):
                     (year, month, day, h, m, s) = readTime(v[0])
                     num = dtToMin(year, month, day, h, m, s)
                     times.append(num)
-                    temps.append(float(
-                        v[6]))  # 2 for thermocouple or 6 for slug
+                    # 2 for thermocouple or 6 for slug
+                    temps.append(float(v[2]))
                     roomTemps.append(float(v[3]))
 
     time0 = times[0]
     ind0 = (times.index(time0))
     temp0 = temps[ind0]
     prev = 0
-    for i in range(1, 6+1):
+    for i in range(1, 6 + 1):
         timeout = i * 10
         t = min(times, key=lambda x: abs(x - (time0 + timeout)))
         ind = times.index(t)
         # dta = (temps[ind]-temps[ind0])/((t-time0))
         dta = abs(temps[ind] - temp0)
-        tError = abs((t-time0)-timeout)
+        tError = abs((t - time0) - timeout)
         # print(tError)
-        if (dta == prev or  tError >= .5): break
+        if (dta == prev or tError >= .5): break
         prev = dta
         dtas.append(dta)
     return (mean(roomTemps), dtas)
 
 
 for fileName in fileNames:
-  try:
-    (roomTemp, dtas) = calc(fileName)
-    print("%s\n\nAverage room temp:\n%f\nDTAs:" % (fileName[13:24], roomTemp))
-    for i in dtas:
-        print(i)
-    print("\n\n\n")
-    # os.remove(fileName)
-  except:
-    print(fileName+" couldn't be read")
+    try:
+        (roomTemp, dtas) = calc(fileName)
+        print("%s\n\nAverage room temp:\n%f\nDTAs:" %
+              (fileName[13:24], roomTemp))
+        for i in dtas:
+            print(i)
+        print("\n\n\n")
+        # os.remove(fileName)
+    except:
+        print(fileName + " couldn't be read")
