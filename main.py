d0c15d39f2a4665f8af3c7d62cc5d5f86ac55a3e
Ibrahim Moghul <34140989+imoghul@users.noreply.github.com>
2021-12-01 19:57:37 +0000

    provides summary of dta averages

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import csv, glob, os, sys
 from numpy import mean
 
+dtToMin = lambda y, mon, d, h, m, s: (525600 * y + 43800 * mon + 1440 * d + 60
+                                      * h + m + s / 60)
 
-def dtToMin(y, mon, d, h, m, s):
-    return (525600 * y + 43800 * mon + 1440 * d + 60 * h + m + s / 60)
+dtasToCalc = 6
+
+
+def average(x):
+    if len(x) == 0: return 0
+    return mean(x)
 
 
 def readTime(dt):  # sample dt: 3:09:12.039 PM 11/24/2021
@@ -50,25 +56,26 @@ def calc(fileName):
     ind0 = (times.index(time0))
     temp0 = temps[ind0]
     prev = 0
-    for i in range(1, 7):
+    for i in range(1, dtasToCalc + 1):
         timeout = i * 10
         t = min(times, key=lambda x: abs(x - (time0 + timeout)))
         ind = times.index(t)
         # dta = (temps[ind]-temps[ind0])/((t-time0))
         dta = abs(temps[ind] - temp0)
-        tError = abs(t - time0) % 10
+        tError = abs(t - time0 - timeout)
         # print(tError)
-        if (dta == prev or (tError <= 9.5 and tError >= .5)): break
+        if (dta == prev or tError >= .6): break
         prev = dta
         dtas.append(dta)
-    return (mean(roomTemps), dtas)
+    return (average(roomTemps), dtas)
 
 
+DTAs = []
 with open("results.csv", mode="w", newline="") as out:
     writer = csv.writer(out)
     writer.writerow([
-        "Test", "File Name", "Average Room Temp.", "DTA1", "DTA2", "DTA3", "DTA4",
-        "DTA5", "DTA6"
+        "Test", "Serial Number", "Date", "Time", "Average Room Temp.", "DTA1",
+        "DTA2", "DTA3", "DTA4", "DTA5", "DTA6"
     ])
     if len(sys.argv) > 1:
         dirs = sys.argv[1:]
@@ -80,16 +87,33 @@ with open("results.csv", mode="w", newline="") as out:
         fileNames = glob.glob("*RAW*.csv", recursive=True)
         fileNames.sort()
         for fileName in fileNames:
-            outlist = []
+            outlist = [dir]
             try:
                 (roomTemp, dtas) = calc(fileName)
-                outlist.append(fileName)
+                DTAs.append(dtas)
+                filelist = fileName.split("_")
+                outlist.append(filelist[1])
+                _date = filelist[3]
+                d = _date[4:6] + "/" + _date[6:] + "/" + _date[0:4]
+                outlist.append(d)
+                outlist.append(filelist[4])
                 outlist.append(roomTemp)
                 for i in dtas:
                     outlist.append(str(i))
-                outlist.insert(0,dir)
                 print(outlist)
                 writer.writerow(outlist)
             except:
                 print(fileName + " couldn't be read")
         os.chdir(original)
+    writer.writerow([])
+
+    avgList = [
+        "",
+        "",
+        "",
+        "",
+        "Average DTAs: ",
+    ]
+    for i in range(dtasToCalc):
+        avgList.append(average([d[i] for d in DTAs if len(d) > i]))
+    writer.writerow(avgList)
