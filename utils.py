23f343735cd3d1a0ef244359df2eac6eaaff4298
Ibrahim Moghul <34140989+imoghul@users.noreply.github.com>
2021-12-28 19:03:36 +0000

    formatted

--- utils.py
+++ utils.py
@@ -10,6 +10,6 @@ dtToMin = lambda y, mon, d, h, m, s: (525600 * y + 43800 * mon + 1440 * d + 60
                                       * h + m + s / 60)
 
 
-def closestTo(arr,val):
-  v = min(arr, key=lambda x: abs(x - val))
-  return (v,arr.index(v))
+def closestTo(arr, val):
+    v = min(arr, key=lambda x: abs(x - val))
+    return (v, arr.index(v))
