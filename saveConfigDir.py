eb37fb657d9ea760deb89a38704b401823e1046b
immoghul <moghul02@gmail.com>
2022-06-23 09:37:37 -0400

    remove junk

--- saveConfigDir.py
+++
@@ -1,5 +0,0 @@
-from tkinter import *
-from tkinter import filedialog
-root = Tk()
-root.withdraw()
-with open("configdir.txt","w") as f:f.write(filedialog.askdirectory(title="Choose the config directory")+"/") 
