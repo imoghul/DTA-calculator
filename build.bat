e173d980209d84660eb946a5cf3efc69a6c9ca4a
immoghul <moghul02@gmail.com>
2022-07-19 14:45:48 -0400

    updated build script and README

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 @REM change directory into the scripts directory
 cd scripts 
 @REM create or update the configdir.txt to contain the config dir that the script will use in the .exe
-python saveConfigDir.py 
+@REM python saveConfigDir.py 
 @REM create the exe including the configdir.txt in it
-python -m PyInstaller -F --add-data "configdir.txt;." --icon="..\deps\phononic_icon.ico" run.py 
+@REM python -m PyInstaller -F --add-data "configdir.txt;." --icon="..\deps\phononic_icon.ico" run.py 
+python -m PyInstaller -F --icon="..\deps\phononic_icon.ico" run.py 
 @REM remove extra .spec file
 del run.spec 
 @REM copy the exe from dist to parent directory
-copy dist\run.exe ..\run.exe 
+copy dist\run.exe ..\run.exe
