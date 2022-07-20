98c865d7ebbb3f02af501c0b8ff151a2e2a3215e
immoghul <moghul02@gmail.com>
2022-07-20 12:46:35 -0400

    updated to ico

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ cd scripts
 @REM python saveConfigDir.py 
 @REM create the exe including the configdir.txt in it
 @REM python -m PyInstaller -F --add-data "configdir.txt;." --icon="..\deps\phononic_icon.ico" run.py 
-python -m PyInstaller -F --icon="..\deps\phononic_icon.png" run.py 
+python -m PyInstaller -F --icon="..\deps\phononic_icon.ico" run.py 
 @REM remove extra .spec file
 del run.spec 
 @REM copy the exe from dist to parent directory
